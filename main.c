b017e921f85262b93c5d1008e63b71b71713b5a6
Florian Heinz <fh@vautron.de>
2022-01-13 12:40:27 +0100

    initial checkin

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <netdb.h>
+
+int calc (char *s) {
+  int result;
+
+  // Calculate the result of the operation here
+
+  return result;
+}
+
+int main (int argc, char **argv) {
+  // Open connection
+
+  // Switch to buffered IO
+
+  // Loop over input lines
+  // Calculate the result (with calc())
+  // Send result back to server
+
+  return 0;
+}
